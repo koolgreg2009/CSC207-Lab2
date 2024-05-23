e5d58920467e066def57242fec02e945c27762fd
kevinhu <kaiyuan.hu@mail.utoronto.ca>
2024-05-23 18:59:21 -0400

    added esfdsfs

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -3,10 +3,10 @@ import java.util.List;
 public class DataTypes {
     // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
-// esfdsfs
-        int s = 0;
+// this expects a list of integers
+        long s = 0;
         // below is a "foreach" loop which iterates through numbers
-        for (int x : numbers) {
+        for (long x : numbers) {
             s += x;
         }
         return s;
